666e744b304591d479207eacc653f63229619e89
ivayloivanof <ivaylo.ivanof@gmail.com>
2016-02-03 20:43:16 +0200

    decide problem 2 call() and applay()

--- 1. Functions-and-Function-Expressions-Homework/homework/functionArguments.js
+++ 1. Functions-and-Function-Expressions-Homework/homework/functionArguments.js
@@ -4,12 +4,24 @@
 			var obj = arguments[i];
 			console.log(obj + ' (' +typeof obj + ')');
 		}
+
+		console.log(this.person + ' ' + this.role);
 	}
 
-	printArgsInfo(2, 3, 2.5, -110.5564, false);
-	printArgsInfo(null, undefined, "", 0, [], {});
-	printArgsInfo([1, 2], ["string", "array"], ["single value"]);
-	printArgsInfo("some string", [1, 2], ["string", "array"], ["mixed", 2, false, "array"], {name: "Peter", age: 20});
-	printArgsInfo([[1, [2, [3, [4, 5]]]], ["string", "array"]]);
+	//printArgsInfo(2, 3, 2.5, -110.5564, false);
+	//printArgsInfo(null, undefined, "", 0, [], {});
+	//printArgsInfo([1, 2], ["string", "array"], ["single value"]);
+	//printArgsInfo("some string", [1, 2], ["string", "array"], ["mixed", 2, false, "array"], {name: "Peter", age: 20});
+	//printArgsInfo([[1, [2, [3, [4, 5]]]], ["string", "array"]]);
+
+	var i = {
+		person: 'Provercho', role: 'Javascript Looser'
+	};
+
+	printArgsInfo.call();
+	printArgsInfo.call(i);
+
+	printArgsInfo.apply();
+	printArgsInfo.apply(i);
 
 })();
